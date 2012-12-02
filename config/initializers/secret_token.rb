cc3045d6afb3314920bbe11faa8553c83985ad23
Will Gretz <Will@WillGretz.com>
2012-12-02 11:04:37 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pathfinder::Application.config.secret_token = '55d3bac67e0a491a8df47121e56cb8e27033f4ad7579a49792fd65dd657a60f25beaa8afc366c8e25bd160ecbaebab84b076d8c52439c5b11e831b55d1e2cdb0'
